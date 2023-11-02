98f93acad63558259a49127c7ce1d114c0049707
Anjali Choudhary <anjalichoudhary@Anjalis-MacBook-Air.local>
2023-11-02 23:11:35 +0530

    Music Player

---
+++ js/music-list.js
@@ -0,0 +1,105 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "AjabSi",
+      artist: "krishna kumar kunnath",
+      img: "ajabsi",
+      src: "AjabSi"
+    },
+    {
+      name: "Asal Mein",
+      artist: "Darshan Raval",
+      img: "asalmein",
+      src: "AsalMein"
+    },
+    {
+      name: "Baarish",
+      artist: "",
+      img: "baarish",
+      src: "Baarish"
+    },
+    {
+      name: "Tera Zikr",
+      artist: "Darshan Raval",
+      img: "terazikr",
+      src: "TeraZikr"
+    },
+    {
+      name: "Voh Lamhe",
+      artist: "",
+      img: "vohlamhe",
+      src: "vohLamhe"
+    },
+    {
+      name: "Ranjha",
+      artist: "",
+      img: "ranjha",
+      src: "Ranjha"
+    },
+    {
+      name: "alag_aasman",
+      artist: "",
+      img: "alagaasman",
+      src: "alagaasman"
+    },
+    {
+      name: "Baarishein",
+      artist: "",
+      img: "baarishein",
+      src: "Baarishein"
+    },
+    {
+      name: "Ishq Wala Love",
+      artist: "",
+      img: "ishqwalalove",
+      src: "IshqWalaLove"
+    },
+    {
+      name: "Kahani Suno",
+      artist: "",
+      img: "kahanisuno",
+      src: "KahaniSuno"
+    },
+    {
+      name: "Mere Sawaal Ka",
+      artist: "",
+      img: "meresawalka",
+      src: "MereSawaalKa"
+    },
+    {
+      name: "Sach Keh Raha Hai",
+      artist: "",
+      img: "sachkehrahah",
+      src: "SachKehrahah"
+    },
+    {
+      name: "Surili Akhiyon Wale",
+      artist: "",
+      img: "suriliakhiyonwale",
+      src: "SuriliAkhiyonWale"
+    },
+    {
+      name: "Tu Chahiye",
+      artist: "",
+      img: "tuchaiye",
+      src: "TuChahiye"
+    },
+    
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
